84f6e97c0fa2c40caf13b15f20cef7b5d7166c63
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2024-07-10 18:38:56 +0200

    add documentation page

---
+++ build/assets/js/a7456010.2e950eea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksnnax_docs=self.webpackChunksnnax_docs||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
