5008158e6d3e3228701c3b09914e751f3c04af5f
Jamie Lohoff <jamie.lohoff@gmail.com>
2024-07-25 22:48:24 -0400

    Added gitignore and modified configuration files for publishing on pypi.

--- setup.py
+++ setup.py
@@ -11,12 +11,6 @@ from setuptools import setup
 if __name__ == "__main__":
     try:
         setup(
-            name="snnax",
-            version="0.0.1",
-            description="A library for spiking neural networks in JAX.",
-            author="Jamie Lohoff, Jan Finkbeiner, Emre Neftci",
-            author_email="jamie.lohoff@gmail.com",
-            packages=["snnax"],
             use_scm_version={"version_scheme": "no-guess-dev"},
             install_requires=["jax", "equinox", "chex"]
         )
