2ad5993f0b0c5dabed13503b19668a873ac551da
Jamie Lohoff <jamie@pool-40-58.vpn.kfa-juelich.de>
2024-07-25 20:41:52 -0400

    Refactored SpikingMaxPool2d.

--- src/snnax/functional/surrogate.py
+++ src/snnax/functional/surrogate.py
@@ -1,38 +1,57 @@
 from typing import Callable
 import jax
+import jax.nn as jnn
 import jax.numpy as jnp
 from jax import custom_jvp
+from chex import Array
 
-from .functional import sigmoid
+
+SpikeFn = Callable[[Array], Array]
 
 
-# TODO set default to 1. or 10. as in 
-# 'The remarkable robustness of surrogate gradient learning 
-# for instilling complex function in spiking neural networks'
-# by Zenke and Vogels (https://www.biorxiv.org/content/10.1101/2020.06.29.176925v1) 
-def superspike_surrogate(beta=10.): # 
+def superspike_surrogate(beta: float = 10.) -> SpikeFn: 
+    """
+    Implementation of the superspike surrogate gradient function as described in
+    'The remarkable robustness of surrogate gradient learning 
+    for instilling complex function in spiking neural networks' by Zenke and 
+    Vogels: (https://www.biorxiv.org/content/10.1101/2020.06.29.176925v1) 
 
+    Arguments:
+        `beta` (float): Parameter to control the steepness of the surrogate 
+            gradient. Default is 10.
+
+    Returns:
+        A function that returns the surrogate gradient of the heaviside function.
+    """
     @custom_jvp
-    def heaviside_with_super_spike_surrogate(x):
-        return jnp.heaviside(x, 1)
+    def heaviside_with_superspike_surrogate(x):
+        return jnp.heaviside(x, 1.)
 
-    @heaviside_with_super_spike_surrogate.defjvp
+    @heaviside_with_superspike_surrogate.defjvp
     def f_jvp(primals, tangents):
         x, = primals
         x_dot, = tangents
-        primal_out = heaviside_with_super_spike_surrogate(x)
-        tangent_out = 1./(beta*jnp.abs(x)+1.) * x_dot
+        primal_out = heaviside_with_superspike_surrogate(x)
+        tangent_out = x_dot / (1. + beta * jnp.abs(x))
         return primal_out, tangent_out
     
-    return heaviside_with_super_spike_surrogate
+    return heaviside_with_superspike_surrogate
 
 
-# TODO set default to 1. or 10. as in 
-# 'The remarkable robustness of surrogate gradient learning 
-# for instilling complex function in spiking neural networks'
-# by Zenke and Vogels (https://www.biorxiv.org/content/10.1101/2020.06.29.176925v1) 
-def sigmoid_surrogate(beta=1.): # set default to 1. or 10. ?
-    assert float(beta) == 1., "Currently only beta = 1.0 is supported for numerical stability."
+def sigmoid_surrogate(beta: float = 1.) -> SpikeFn:
+    """
+    Implementation of the sigmoidal surrogate gradient function as described in
+    'The remarkable robustness of surrogate gradient learning 
+    for instilling complex function in spiking neural networks' by Zenke and 
+    Vogels: (https://www.biorxiv.org/content/10.1101/2020.06.29.176925v1) 
+
+    Arguments:
+        `beta` (float): Parameter to control the steepness of the surrogate 
+            gradient. Default is 10.
+
+    Returns:
+        A function that returns the surrogate gradient of the heaviside function.
+    """
     @custom_jvp
     def heaviside_with_sigmoid_surrogate(x):
         return jnp.heaviside(x, 1.)
@@ -43,12 +62,26 @@ def sigmoid_surrogate(beta=1.): # set default to 1. or 10. ?
         x_dot, = tangents
         primal_out = heaviside_with_sigmoid_surrogate(x)
         # TODO multiplication by beta correct here?
-        tangent_out = sigmoid(x, beta) * (1.-beta*sigmoid(x, beta)) * x_dot 
+        tangent_out = jnn.sigmoid(x, beta) 
+        tangent_out *= (1. - beta * jnn.sigmoid(x, beta)) * x_dot 
         return primal_out, tangent_out
 
     return heaviside_with_sigmoid_surrogate
 
-def piecewise_surrogate(beta=.5): # set default to 1. or 10. ?
+def piecewise_surrogate(beta: float = .5) -> SpikeFn:
+    """
+    Implementation of the sigmoidal surrogate gradient function as described in
+    'The remarkable robustness of surrogate gradient learning 
+    for instilling complex function in spiking neural networks' by Zenke and 
+    Vogels: (https://www.biorxiv.org/content/10.1101/2020.06.29.176925v1) 
+
+    Arguments:
+        `beta` (float): Parameter to control the steepness of the surrogate 
+            gradient. Default is .5.
+
+    Returns:
+        A function that returns the surrogate gradient of the heaviside function.
+    """
     @custom_jvp
     def heaviside_with_sigmoid_surrogate(x):
         return jnp.heaviside(x, 1.)
--- src/snnax/snn/layers/maxpool2d.py
+++ src/snnax/snn/layers/maxpool2d.py
@@ -1,29 +1,37 @@
+from typing import Callable, Optional, Sequence, Union, Tuple
+
 import jax
 import jax.numpy as jnp
 import equinox as eqx
-from .stateful import RequiresStateLayer
-from typing import Sequence, Union, Tuple, Any, Callable, Optional
 from equinox import static_field
-from ...functional.surrogate import superspike_surrogate
+from chex import Array, PRNGKey
 
-class MaxPool2d(eqx.nn.MaxPool2d, RequiresStateLayer):
+from .stateful import RequiresStateLayer
+from ...functional.surrogate import superspike_surrogate, SpikeFn
+
+
+class SpikingMaxPool2d(eqx.nn.MaxPool2d, RequiresStateLayer):
     """
     Simple module to flatten the output of a layer.
+
+    Arguments:
+        `threshold` (Union[float, Array]): Threshold of the spiking neuron.
+        `spike_fn` (SpikeFn): Surrogate gradient function for the spiking neuron.
     """
-    spike_fn: Callable[[jnp.ndarray], jnp.ndarray] = static_field()
-    threshold: Union[float, jnp.ndarray] = static_field()
-    
-    def __init__(self, *args, spike_fn = superspike_surrogate(10.0), threshold = 1.0, **kwargs, ):
+    threshold: Union[float, Array] = static_field()
+    spike_fn: SpikeFn = static_field()
+
+    def __init__(self, 
+                *args,
+                threshold: Union[float, Array] = 1.0, 
+                spike_fn: SpikeFn = superspike_surrogate(10.0), 
+                **kwargs):
         self.threshold = threshold
-        
         self.spike_fn = spike_fn
             
         super().__init__(*args, **kwargs)
     
-    def __call__(self, x, key: Optional[jax.random.PRNGKey] = None):
+    def __call__(self, x, key: Optional[PRNGKey] = None):
         out = super().__call__(x) 
         return self.spike_fn(out-self.threshold)
-    
-    
-    
 
