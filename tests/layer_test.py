e87ffddae5cd587b18fb9206f57f36f7cd346297
Catherine Schöfmann <c.schoefmann@fz-juelich.de>
2024-08-01 11:22:24 +0200

    Switch to pytest, parameterize

--- tests/layer_test.py
+++ tests/layer_test.py
@@ -1,128 +1,44 @@
-import unittest
-
-import jax
+import equinox as eqx
 import jax.numpy as jnp
 import jax.random as jrand
-
-import equinox as eqx
+import pytest
 
 import snnax.snn as snn
-from snnax.utils.tree import is_decay_constants
 from snnax.utils.filter import filter_grad
+from snnax.utils.tree import is_decay_constants
 
 
-class TestLayer(unittest.TestCase):
-    def test_simple_lif(self):
-        key = jrand.PRNGKey(42)
-        layer = snn.SimpleLIF([0.95])
-        shape = (16, 16)
-        init_state = layer.init_state(shape, key=key)
-        self.assertTrue(all(s.shape == shape for s in init_state))
+@pytest.mark.parametrize("layer", [
+    snn.SimpleLIF([0.95]),
+    snn.LIF([0.95, 0.85]),
+    snn.IAF([0.95], [0.85]),
+    snn.SimpleLI([0.95]),
+])
+def test_layer(layer):
+    key = jrand.PRNGKey(42)
+    shape = (16, 16)
+    init_state = layer.init_state(shape, key=key)
+    assert all(s.shape == shape for s in init_state)
 
-        input_spikes = jrand.uniform(key, shape)
-        input_spikes = jnp.where(input_spikes > 0.25, 1., 0.)
-        state, output = layer(init_state, input_spikes)
-        self.assertTrue(output.shape == shape)
+    input_spikes = jrand.uniform(key, shape)
+    input_spikes = jnp.where(input_spikes > 0.25, 1., 0.)
+    state, output = layer(init_state, input_spikes)
+    assert output.shape == shape
 
-        def _test_grad(layer, state, input_spikes):
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            return jnp.sum(state[-1])
-        
-        grad_fn = eqx.filter_grad(_test_grad)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(jnp.sum(grads.decay_constants) != 0)
+    def _test_grad(layer, state, input_spikes):
+        state, output = layer(state, input_spikes)
+        state, output = layer(state, input_spikes)
+        state, output = layer(state, input_spikes)
+        return jnp.sum(state[-1])
 
-        grad_fn = filter_grad(_test_grad, filter_spec=is_decay_constants)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(grads.decay_constants is None)
+    grad_fn = eqx.filter_grad(_test_grad)
+    grads = grad_fn(layer, init_state, input_spikes)
+    assert jnp.sum(grads.decay_constants) != 0
 
-    def test_lif(self):
-        key = jrand.PRNGKey(42)
-        layer = snn.LIF([0.95, 0.85])
-        shape = (16, 16)
-        init_state = layer.init_state(shape, key=key)
-        self.assertTrue(all(s.shape == shape for s in init_state))
-
-        input_spikes = jrand.uniform(key, shape)
-        input_spikes = jnp.where(input_spikes > 0.25, 1., 0.)
-        state, output = layer(init_state, input_spikes)
-        self.assertTrue(output.shape == shape)
-
-        def _test_grad(layer, state, input_spikes):
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            return jnp.sum(state[-1])
-        
-        grad_fn = eqx.filter_grad(_test_grad)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(jnp.sum(grads.decay_constants) != 0)
+    grad_fn = filter_grad(_test_grad, filter_spec=is_decay_constants)
+    grads = grad_fn(layer, init_state, input_spikes)
+    assert grads.decay_constants is None
 
 
-
-        grad_fn = filter_grad(_test_grad, filter_spec=is_decay_constants)
-
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(grads.decay_constants is None)
-
-    def test_iaf(self):
-        key = jrand.PRNGKey(42)
-        layer = snn.IAF([0.95], [0.85])
-        shape = (16, 16)
-        init_state = layer.init_state(shape, key=key)
-        self.assertTrue(all(s.shape == shape for s in init_state))
-
-        input_spikes = jrand.uniform(key, shape)
-        input_spikes = jnp.where(input_spikes > 0.25, 1., 0.)
-        state, output = layer(init_state, input_spikes)
-        self.assertTrue(output.shape == shape)
-
-        def _test_grad(layer, state, input_spikes):
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            return jnp.sum(state[-1])
-        
-        grad_fn = eqx.filter_grad(_test_grad)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(jnp.sum(grads.decay_constants) != 0)
-
-        grad_fn = filter_grad(_test_grad, filter_spec=is_decay_constants)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(grads.decay_constants is None)
-
-    def test_li(self):
-        key = jrand.PRNGKey(42)
-        layer = snn.SimpleLI([0.95])
-        shape = (16, 16)
-        init_state = layer.init_state(shape, key=key)
-        self.assertTrue(all(s.shape == shape for s in init_state))
-
-        input_spikes = jrand.uniform(key, shape)
-        input_spikes = jnp.where(input_spikes > 0.25, 1., 0.)
-        state, output = layer(init_state, input_spikes)
-        self.assertTrue(output.shape == shape)
-
-        def _test_grad(layer, state, input_spikes):
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            state, output = layer(state, input_spikes)
-            return jnp.sum(state[-1])
-        
-        grad_fn = eqx.filter_grad(_test_grad)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(jnp.sum(grads.decay_constants) != 0)
-
-        grad_fn = filter_grad(_test_grad, filter_spec=is_decay_constants)
-        grads = grad_fn(layer, init_state, input_spikes)
-        self.assertTrue(grads.decay_constants is None)
-        
-
-    def test_srm(self):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_srm():
+    pass
--- tests/tree_test.py
+++ tests/tree_test.py
@@ -1,52 +1,42 @@
-import unittest
-
-import jax
+import equinox as eqx
+import equinox.nn as nn
 import jax.numpy as jnp
 import jax.random as jrand
 
-import equinox as eqx
-import equinox.nn as nn
-
-import snnax as snx
 import snnax.snn as snn
 from snnax.snn.architecture import default_forward_fn
 
 
-class TestArchitecture(unittest.TestCase):
-    def test_treshold(self):
-        print("Testing default_forward_fn and gradient computation...")
-        key = jrand.PRNGKey(42)
-        keys = jrand.split(key, 4)
-        time_steps = 25
+def test_threshold():
+    print("Testing default_forward_fn and gradient computation...")
+    key = jrand.PRNGKey(42)
+    keys = jrand.split(key, 4)
+    time_steps = 25
 
-        model = snn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=(3, 3), use_bias=False, key=keys[0]),
-            snn.LIF([.95, .85], threshold=jnp.ones(()), shape=(16, 5, 5), key=keys[1]),
+    model = snn.Sequential(
+        nn.Conv2d(3, 16, kernel_size=(3, 3), use_bias=False, key=keys[0]),
+        snn.LIF([.95, .85], threshold=jnp.ones(()), shape=(16, 5, 5), key=keys[1]),
 
-            nn.Conv2d(16, 16, kernel_size=(3, 3), use_bias=False, key=keys[2]),
-            snn.LIF([.95, .85], shape=(16, 3, 3), key=keys[3]),
+        nn.Conv2d(16, 16, kernel_size=(3, 3), use_bias=False, key=keys[2]),
+        snn.LIF([.95, .85], shape=(16, 3, 3), key=keys[3]),
 
-            snn.Flatten(),
-            nn.Linear(16*3*3, 5, use_bias=False, key=keys[4]),
-            snn.LIF([.95, .85], shape=(5,), key=keys[5]),
-            forward_fn=default_forward_fn
-        )
+        snn.Flatten(),
+        nn.Linear(16 * 3 * 3, 5, use_bias=False, key=keys[4]),
+        snn.LIF([.95, .85], shape=(5,), key=keys[5]),
+        forward_fn=default_forward_fn
+    )
 
-        input_spikes = jrand.uniform(key, (32, time_steps, 3, 7, 7))
-        input_spikes = jnp.where(input_spikes > 0.3, 1., 0.)
+    input_spikes = jrand.uniform(key, (32, time_steps, 3, 7, 7))
+    input_spikes = jnp.where(input_spikes > 0.3, 1., 0.)
 
-        states = model.init_state((3, 7, 7), key=key)
-        batched_model = eqx.filter_vmap(model, in_axes=(None, 0, None))
+    states = model.init_state((3, 7, 7), key=key)
+    batched_model = eqx.filter_vmap(model, in_axes=(None, 0, None))
 
-        def dummy_loss_fn(model, states, input_spikes, key):
-            states, out = model(states, input_spikes, key)
-            return jnp.sum(out[-1])
+    def dummy_loss_fn(model, states, input_spikes, key):
+        states, out = model(states, input_spikes, key)
+        return jnp.sum(out[-1])
 
-        grads = eqx.filter_grad(dummy_loss_fn)(batched_model, states, input_spikes, key)
-        print(grads._fun.layers[1].threshold)
-        self.assertTrue(jnp.abs(grads._fun.layers[1].threshold).sum() != 0)
+    grads = eqx.filter_grad(dummy_loss_fn)(batched_model, states, input_spikes, key)
+    print(grads._fun.layers[1].threshold)
 
-
-if __name__ == '__main__':
-    unittest.main()
-
+    assert jnp.abs(grads._fun.layers[1].threshold).sum() != 0
