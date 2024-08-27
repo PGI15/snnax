601d0dc596d04a0f0da2617dfc13ae87d12c3fc7
anil <anil01kaya@gmail.com>
2024-08-27 15:17:01 +0300

    Leaky Integrate layer documentation is improved.

--- src/snnax/snn/layers/li.py
+++ src/snnax/snn/layers/li.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Union, Sequence
 
 from jax.lax import clamp
 
@@ -18,10 +18,10 @@ class SimpleLI(StatefulLayer):
             is provided.
         `shape` (StateShape): Shape of the state of the layer.
     """
-    decay_constants: Array
+    decay_constants: Union[Sequence[float], Array]
 
     def __init__(self,
-                decay_constants: Array,
+                decay_constants: Union[Sequence[float], Array],
                 init_fn: Optional[Callable] = default_init_fn,
                 shape: Optional[StateShape] = None,
                 key: Optional[PRNGKey] = None,) -> None:
@@ -32,6 +32,21 @@ class SimpleLI(StatefulLayer):
                 state: Array, 
                 synaptic_input: Array, *, 
                 key: Optional[PRNGKey] = None) -> StatefulOutput:
+        """
+        Takes the current layer state variables and synaptic input for the 
+        given timestep and generates the layer states and membrane potential values 
+        for the next timestep.
+        
+        Parameters:
+            `state` (Array): Contains membrane potential state variable array, 
+                which is the only state variable for the Leaky Integrator layer.
+            `synaptic_input` (Array): Input values for the layer.
+            `key` (Optional[PRNGKey]): Defaults to None. Random number generator key.
+
+        Returns:
+            `[0]` (Sequence[Array]): New state of the layer. Contains only membrane potential.
+            `[1]` (Array): Membrane potential state variable.
+        """
         alpha = clamp(0.5, self.decay_constants[0], 1.0)
         mem_pot = state[0]
         mem_pot = alpha*mem_pot + (1.-alpha)*synaptic_input
